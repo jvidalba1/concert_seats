9536532056b5047d14d009d9cbdfa1fb99d7d643
Mateo Vidal <jvidalba@gmail.com>
2020-05-11 21:05:37 -0500

    Add venue controller

---
+++ app/controllers/venues_controller.rb
@@ -0,0 +1,17 @@
+class VenuesController < ApplicationController
+
+  # Best available seats
+  def best_seats
+
+  end
+
+  private
+
+  def venue_params
+    params.require(:venue).permit(:rows, :columns)
+  end
+
+  def seat_params
+    params.require(:seats).permit(:id, :row, :column, :status)
+  end
+end
