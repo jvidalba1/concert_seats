42fb2c78f1fa921b0bc239d77937553bd05e5f68
Mateo Vidal <jvidalba@gmail.com>
2020-05-13 11:58:29 -0500

    Center best two seats when columns number is even

--- solution.rb
+++ solution.rb
@@ -37,7 +37,9 @@ def best_available_seat(json_parameter)
   rows = venue_params["layout"]["rows"]
   columns = venue_params["layout"]["columns"]
 
+  even = false
   columns_peak = if columns % 2 == 0
+    even = true
     columns / 2
   else
     columns / 2 + 1
@@ -46,15 +48,16 @@ def best_available_seat(json_parameter)
   letter = "a"
   index_column = 1
   increment_index_column = true
-  byebug
 
-  oelo = (rows..1).each_with_object({}) do |row, hsh|
-    (1..columns).each_with_index do |column, index|
+  (1..rows).to_a.reverse.each_with_object({}) do |row, hsh|
+    (1..columns).to_a.each_with_index do |column, index|
 
       hsh[letter + (index + 1).to_s] = row + index_column
 
-      if increment_index_column && index_column < columns_peak #diferenciar de columnas impar o par <= o <
+      if index_column < columns_peak && increment_index_column
         index_column += 1
+      elsif even && increment_index_column && index_column == columns_peak #diferenciar de columnas impar o par <= o <
+        increment_index_column = false
       else
         increment_index_column = false
         index_column -= 1
@@ -65,10 +68,8 @@ def best_available_seat(json_parameter)
     increment_index_column = true
     letter = letter.next
   end
-
-  return oelo
 end
 
-json = '{"venue": { "layout": { "rows": 10, "columns": 50 } }, "seats": { "a1": { "id": "a1", "row": "a", "column": 1, "status": "AVAILABLE" }, "b5": { "id": "b5", "row": "b", "column": 5, "status": "AVAILABLE" }, "h7": { "id": "h7", "row": "h", "column": 7, "status": "AVAILABLE"  } } }'
+json = '{"venue": { "layout": { "rows": 2, "columns": 6 } }, "seats": { "a1": { "id": "a1", "row": "a", "column": 1, "status": "AVAILABLE" }, "b5": { "id": "b5", "row": "b", "column": 5, "status": "AVAILABLE" }, "h7": { "id": "h7", "row": "h", "column": 7, "status": "AVAILABLE"  } } }'
 
 p best_available_seat(json)
